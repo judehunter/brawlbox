79a5a7a9143c60c8c2f1cfe30b0d785737480973
Jakub Kopańko <jakub@kopanko.com>
2020-03-21 18:52:44 +0100

    Revert "Set up proper pixel rendering"
    This reverts commit b5bb56158a7049d0b0f35d89e58df76aecaeda35.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Nie chwal dnia przed zachodem słońca")]
+[assembly: AssemblyTitle("Nie chwal dnia przed zachodem slonca")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
