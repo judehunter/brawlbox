1ce477bcfe78cb61366f87a3b7b90b2617ccedea
judehunter <matisowagm@gmail.com>
2020-03-21 18:24:23 +0100

    Example script

--- Player.cs
+++ Player.cs
@@ -3,19 +3,20 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		var move = new Vector2();
+		if (Input.IsActionPressed("ui_right")) move.x += 1;
+		if (Input.IsActionPressed("ui_left")) move.x -= 1;
+		if (Input.IsActionPressed("ui_up")) move.y -= 1;
+		if (Input.IsActionPressed("ui_down")) move.y += 1;
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+		Position += move.Normalized();
+	}
 }
