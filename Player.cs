da64da8842c6f3c771b425ce2ed6c44f8d3ab850
judehunter <matisowagm@gmail.com>
2020-03-22 22:08:06 +0100

    Snappy jumping

--- Player.cs
+++ Player.cs
@@ -3,17 +3,41 @@ using System;
 
 public class Player : KinematicBody2D
 {
-	[Export]
-	readonly float moveSpeed;
+	[Export] readonly float moveSpeed;
+	[Export] readonly float jumpStrength;
+	[Export] readonly float gravityStrength = 1;
+	[Export] readonly float gravityFallStrength = 2;
+
+	bool isJump = false;
+	Vector2 velocity;
+
+	void GetInput()
+	{
+		velocity.x = (Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left")) * moveSpeed;
+		if (velocity.y == 0)
+		{
+			if (Input.IsActionPressed("move_up"))
+			{
+				isJump = true;
+				velocity.y = -1 * jumpStrength;
+			}
+		}
+		if (Input.IsActionJustReleased("move_up") || velocity.y > 0)
+		{
+			isJump = false;
+		}
+	}
+
+	void ApplyGravity()
+	{
+		velocity.y += 9.8f * (isJump ? gravityStrength : gravityFallStrength);
+	}
+		
 
 	public override void _PhysicsProcess(float delta)
 	{
-		var move = new Vector2
-		{
-			x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
-			y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up")
-		};
-
-		Position += move.Normalized() * delta * moveSpeed;
+		GetInput();
+		ApplyGravity();
+		velocity = MoveAndSlide(velocity, Vector2.Up);
 	}
 }
