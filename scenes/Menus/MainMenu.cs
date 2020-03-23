d7f739a2a28328a732c107ab80cab0a4e0002916
Jakub Kopańko <jakub@kopanko.com>
2020-03-23 21:04:14 +0100

    Add basic main menu and implement server broadcast listener

---
+++ scenes/Menus/MainMenu.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public class MainMenu : VBoxContainer
+{
+
+	public override void _Ready()
+	{
+		
+	}
+
+	private void _on_Solo_pressed()
+	{
+		GD.Print("ee");
+	}
+
+}
+
+
+
--- utils/FpsCounter.cs
+++ utils/FpsCounter.cs
@@ -17,4 +17,3 @@ public class FpsCounter : CanvasLayer
 		label.Text = "FPS: " + Engine.GetFramesPerSecond();
 	}
 }
-0
