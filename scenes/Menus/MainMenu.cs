65032ad0c5a692a9cf18b7097223b72e1d560eaa
Jakub Kopańko <jakub@kopanko.com>
2020-03-24 12:31:38 +0100

    Added menu sounds and server address box

--- scenes/Menus/MainMenu.cs
+++ scenes/Menus/MainMenu.cs
@@ -1,17 +1,40 @@
 using Godot;
 using System;
 
-public class MainMenu : VBoxContainer
+public class MainMenu : MarginContainer
 {
+	AudioStreamPlayer select;
 
 	public override void _Ready()
 	{
-		
+		GetNode<HBoxContainer>("VBoxContainer/UI/Right/Multiplayer").Visible = false;
+		select = GetNode<AudioStreamPlayer>("SelectSounds");
 	}
 
 	private void _on_Solo_pressed()
 	{
 		GD.Print("ee");
+		select.Play();
+	}
+
+	private void _on_Duo_pressed()
+	{
+		GetNode<HBoxContainer>("VBoxContainer/UI/Right/Multiplayer").Visible = true;
+		select.Play();
+	}
+
+	private void _on_JoinServer_pressed()
+	{
+		GetNode<CenterContainer>("AddressBox").Visible = true;
+		select.Play();
+	}
+
+	private void _on_AddressBox_gui_input(InputEvent @event)
+	{
+		if (@event.IsPressed())
+		{
+			GetNode<CenterContainer>("AddressBox").Visible = false;
+		}
 	}
 
 }
